daf5c67d87ee719ca2ee884dbb92ae0349bdeecc
ThePaftDunk <100831428+ThePaftDunk@users.noreply.github.com>
2022-03-15 04:34:34 +0530

    Create CoreDigit.scala

---
+++ src/main/scala/CoreDigit.scala
@@ -0,0 +1,10 @@
+package com.test.FirstScala
+
+object CoreDigit extends App {
+
+  val lines = io.Source.fromFile("src/main/conf/CoreDigit.txt").getLines().toList(0).split(" ")
+  val digits = lines(0).toList.map(_.toString.toInt).sum
+  val total = ((digits.toLong * lines(1).toLong).toLong)
+  def rec (num:Long): Long = if (num == 0) 0 else if (num < 10) num else (num % 9)
+  println(rec(total))
+}
