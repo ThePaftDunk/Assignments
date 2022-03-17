afad43b8f3b76afc3ab29608698cebb78d466c24
ThePaftDunk <100831428+ThePaftDunk@users.noreply.github.com>
2022-03-17 11:25:19 +0530

    Update CoreDigit.scala

--- src/main/scala/CoreDigit.scala
+++ src/main/scala/CoreDigit.scala
@@ -5,6 +5,6 @@ object CoreDigit extends App {
   val lines = io.Source.fromFile("src/main/conf/CoreDigit.txt").getLines().toList(0).split(" ")
   val digits = lines(0).toList.map(_.toString.toInt).sum
   val total = ((digits.toLong * lines(1).toLong).toLong)
-  val rec = (num:Long) => if (num == 0) 0 else if (num < 10) num else (num % 9)  
+  val rec = (num:Long) => num % 9  
   println(rec(total)) // Output is 3
 }
