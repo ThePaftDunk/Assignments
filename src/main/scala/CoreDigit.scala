9883ec3733704a704467aeacda55a784546df36f
ThePaftDunk <100831428+ThePaftDunk@users.noreply.github.com>
2022-03-17 12:32:07 +0530

    Update CoreDigit.scala
    changing it to if else condition

--- src/main/scala/CoreDigit.scala
+++ src/main/scala/CoreDigit.scala
@@ -5,6 +5,7 @@ object CoreDigit extends App {
   val lines = io.Source.fromFile("src/main/conf/CoreDigit.txt").getLines().toList(0).split(" ")
   val digits = lines(0).toList.map(_.toString.toInt).sum
   val total = ((digits.toLong * lines(1).toLong).toLong)
-  val rec = (num:Long) => num % 9  
-  println(rec(total)) // Output is 3
+  println(total)
+  val rec = (num:Long) => if (num == 0) 0 else num % 9
+  println(rec(total))
 }
