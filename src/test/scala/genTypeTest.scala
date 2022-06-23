b0de03a65906de30b5b0656e96399d7aec185135
ThePaftDunk <100831428+ThePaftDunk@users.noreply.github.com>
2022-06-23 18:16:47 +0530

    Update genTypeTest.scala

--- src/test/scala/genTypeTest.scala
+++ src/test/scala/genTypeTest.scala
@@ -9,7 +9,7 @@ class genTypeTest extends AnyFunSuite{
   val inputList = List((List(4,6,8,10),8, 2, 4), (List(10,20,30,40,50),40,3,5), (List(-12,-8,24,26,28),26,3,5),
     (List(15,25,26,27,28,29,35,45),35,6,8), (List(4,5,6,7,8,9),2,-1,6))
 
-  val inputArrBuf = List(ArrayBuffer(-3,54,4,6,8,9), ArrayBuffer(10,8,4,6,3,7,4), ArrayBuffer(3,4,6,7,8,10))
+  val inputArrBuf = List(ArrayBuffer(-3,-4,4,6,8,9), ArrayBuffer(10,8,4,6,3,7,4), ArrayBuffer(3,4,6,7,8,10))
 
   test("genTypeTest.BinarySearch.binarySearch") {
     inputList.foreach( list =>
@@ -18,7 +18,7 @@ class genTypeTest extends AnyFunSuite{
 
   test("genTypeTest.CustomSorted") {
     inputArrBuf.foreach(aBuf =>
-      assert(com.Assignment.genType.CustomSorted(aBuf).customSort === aBuf.sorted)
+      assert(aBuf.toList.sorted === com.Assignment.genType.CustomSorted(aBuf).customSort.toList)
     )
   }
 
