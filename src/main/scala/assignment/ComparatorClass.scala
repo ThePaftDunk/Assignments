fb28420e01a4b1e2424bc3c1c4b7bc54a2a811df
ThePaftDunk <100831428+ThePaftDunk@users.noreply.github.com>
2022-06-27 12:23:37 +0530

    Update ComparatorClass.scala
    making Comparator class generic

--- src/main/scala/assignment/ComparatorClass.scala
+++ src/main/scala/assignment/ComparatorClass.scala
@@ -1,12 +1,12 @@
 package com.test.FirstScala
 package com.Assignment
 
-class ComparatorClass extends Comparator[Int]{
+class ComparatorClass[A] (implicit ord: Ordering[A]) extends Comparator[A]{
 
-  def compare(o1: Int, o2: Int): Int = {
-    (o1, o2) match {
-      case i if o1 < o2 => -1
-      case i if o1 > o2 => 1
+   def compare(o1: A, o2: A): Int = {
+     (o1, o2) match {
+      case i if ord.lt(o1 , o2) => -1
+      case i if ord.gt(o1 , o2) => 1
       case _ if o1 == o2 => 0
     }
   }
